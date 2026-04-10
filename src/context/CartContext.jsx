06260e42d2af98401c55be1ff7a0aec16c421bdc
derek <derekdvalerio@gmail.com>
2026-04-10 13:46:45 +0800

    added: global CartContext

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useReducer } from "react";
+import { cartReducer, initialState } from "../features/cart/cartReducer.js";
+
+//a global state container that lets components access and update data without prop drilling
+const CartContext = createContext();
+
+  const [state, dispatch] = useReducer(cartReducer, initialState);
+
+  return (
+    <CartContext.Provider value={{ state, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export default CartContext;
