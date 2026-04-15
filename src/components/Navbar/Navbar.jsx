09bdfed05ffd07579c8a27c6857bc079feca9e89
derek <derekdvalerio@gmail.com>
2026-04-15 20:04:30 +0800

    added: navbar skeleton

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>fraGrenade</h1>
+      <ul>
+        <li>Home</li>
+        <li>Shop</li>
+        <li>Cart</li>
+      </ul>
+      <button type=""></button>
+    </nav>
+  );
+}
