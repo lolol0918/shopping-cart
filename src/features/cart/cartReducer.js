5def5e99fe4fd23c3088b55a0b902280b1cea608
derek <derekdvalerio@gmail.com>
2026-04-17 16:53:06 +0800

    chore: fix linting

--- src/features/cart/cartReducer.js
+++ src/features/cart/cartReducer.js
@@ -4,7 +4,7 @@ export const initialState = [];
 //passing the contents of the cart and what action it will do e.g add items, remove items, or clear items.
 export function cartReducer(state, action) {
   switch (action.type) {
-    case "ADD_ITEM":
+    case "ADD_ITEM": {
       // check if the item is in the shopping cart
       const existing = state.find((item) => item.id === action.payload.id);
       if (existing) {
@@ -17,16 +17,19 @@ export function cartReducer(state, action) {
       } else {
         return [...state, { ...action.payload }];
       }
+    }
 
-    case "REMOVE_ITEM":
+    case "REMOVE_ITEM": {
       return state.filter((item) => item.id !== action.payload.id);
+    }
 
-    case "UPDATE_QUANTITY":
+    case "UPDATE_QUANTITY": {
       return state.map((item) =>
         item.id === action.payload.id
           ? { ...item, quantity: action.payload.quantity }
           : item,
       );
+    }
 
     case "CLEAR_CART":
       return [];
