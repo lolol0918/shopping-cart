2f916dbf8e5ee530415fa37a4507c10687d55bd2
derek <derekdvalerio@gmail.com>
2026-04-17 16:54:06 +0800

    feat: working routes on pages
    -but it still is not linked to the navbar buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import Navbar from "./components/Navbar/Navbar.jsx";
-import "./App.css";
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar.jsx';
+
+import Shop from './pages/Shop.jsx';
+import Cart from './pages/Cart.jsx';
+import Home from './pages/Home.jsx';
+import './App.css';
 
 function App() {
-  return <Navbar></Navbar>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
