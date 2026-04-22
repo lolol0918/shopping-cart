cd4619aa37b79eec9ef1b53f6db6bba8a14b9430
derek <derekdvalerio@gmail.com>
2026-04-22 14:45:06 +0800

    added(services): fetching from fakestore API

---
+++ src/services/api.js
@@ -0,0 +1,26 @@
+async function getProducts() {
+  try {
+    // Wait for the network request to complete
+    const res = await fetch('https://fakestoreapi.com/products');
+
+    // Manually check for http errors
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await res.json();
+
+    // normalizing the structure
+    return data.map((item) => ({
+      id: item.id,
+      name: item.title,
+      price: item.price,
+      image: item.image,
+      description: item.description,
+      category: item.category,
+    }));
+  } catch (error) {
+    console.error('Fetch failed:', error);
+    return null;
+  }
+}
