a0552ad28e5b0250c06423f596831103c4580072
derek <derekdvalerio@gmail.com>
2026-04-25 10:53:27 +0800

    feat: added caching for cart

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,11 +1,26 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useReducer, useEffect } from 'react';
 import { cartReducer, initialState } from '../features/cart/cartReducer.js';
 
 //a global state container that lets components access and update data without prop drilling
 export const CartContext = createContext();
 
+function getInitialCart() {
+  const cached = localStorage.getItem('cart');
+
+  if (cached) {
+    return JSON.parse(cached);
+  }
+
+  return initialState;
+}
+
 export default function CartProvider({ children }) {
-  const [state, dispatch] = useReducer(cartReducer, initialState);
+  const [state, dispatch] = useReducer(cartReducer, getInitialCart());
+
+  //changes the localStorage every time the state changes
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(state));
+  }, [state]);
 
   return (
     <CartContext.Provider value={{ state, dispatch }}>
